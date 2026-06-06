fef3ad334d31c488e272840d9caf54366a4e9aa4
shreechandansamal <shreechandansamal@gmail.com>
2026-06-06 10:00:03 +0530

    Restore CubeIDE metadata and add USART driver skeleton

---
+++ drivers/Inc/stm32f446xx_usart_driver.h
@@ -0,0 +1,13 @@
+/*
+ * stm32f446xx_usart_driver.h
+ *
+ *  Created on: Jun 5, 2026
+ *      Author: chandan
+ */
+
+#ifndef INC_STM32F446XX_USART_DRIVER_H_
+#define INC_STM32F446XX_USART_DRIVER_H_
+
+
+
+#endif /* INC_STM32F446XX_USART_DRIVER_H_ */
---
+++ drivers/Src/stm32f446xx_usart_driver.c
@@ -0,0 +1,8 @@
+/*
+ * stm32f446xx_usart_driver.c
+ *
+ *  Created on: Jun 5, 2026
+ *      Author: chandan
+ */
+
+
